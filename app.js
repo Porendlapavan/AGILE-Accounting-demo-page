258ccc191a17e45efd29ecadad3fac70498c4dee
Pavan Porendla <89634035+Porendlapavan@users.noreply.github.com>
2022-06-12 18:49:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,36 @@ function classToggle() {
 
 document.querySelector('.toggle-buttons')
 	.addEventListener('click', classToggle);
+
+
+function myfunction1() {
+	document.getElementById("myDropdown").classList.toggle("show");
+	if (document.getElementById("myDropdown1").classList.contains("show")) {
+		document.getElementById("myDropdown1").classList.remove("show");
+	}
+	if (document.getElementById("myDropdown2").classList.contains("show")) {
+		document.getElementById("myDropdown2").classList.remove("show");
+	}
+
+}
+
+function myfunction2() {
+	document.getElementById("myDropdown1").classList.toggle("show");
+	if (document.getElementById("myDropdown").classList.contains("show")) {
+		document.getElementById("myDropdown").classList.remove("show");
+	}
+	if (document.getElementById("myDropdown2").classList.contains("show")) {
+		document.getElementById("myDropdown2").classList.remove("show");
+	}
+
+}
+
+function myfunction3() {
+	document.getElementById("myDropdown2").classList.toggle("show");
+	if (document.getElementById("myDropdown1").classList.contains("show")) {
+		document.getElementById("myDropdown1").classList.remove("show");
+	}
+	if (document.getElementById("myDropdown").classList.contains("show")) {
+		document.getElementById("myDropdown").classList.remove("show");
+	}
+}
